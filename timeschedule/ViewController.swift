b9ea6560f4e26162e6a4fbb06bad1f0652b3dfa7
Masahiro Tonogai <tonomasa36@gmail.com>
2020-08-29 16:25:17 +0900

    最初

---
+++ timeschedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timeschedule
+//
+//  Created by masahiro tono on 2020/08/29.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
