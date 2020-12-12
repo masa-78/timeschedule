ab516c4a1782d488ce93d18049dfd5a23b334dc6
Masahiro Tonogai <tonomasa36@gmail.com>
2020-12-12 20:33:22 +0900

    12月12日スクール終わり

--- timeschedule/NyuryokuViewController.swift
+++ timeschedule/NyuryokuViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class NyuryokuViewController: UIViewController, UITextFieldDelegate , UITableViewDataSource , UITableViewDelegate {
+    
+    func ClassItem() {
+        var _ : String
+        var _: Bool = false
+//        init(title: String) {
+//        self.title = title
+//        }
+
+    }
+    init(title: String) {
+    self.title = title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var itemArray: [Item] = []
 
     @IBOutlet var table: UITableView!
     
@@ -29,12 +47,12 @@ class NyuryokuViewController: UIViewController, UITextFieldDelegate , UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell",for: indexPath) as! CustomTableViewCell
         
-        tableView(_, tableView: UITableView, commit, editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-            
-            itemArray.remove(at: indexPath.row)
-            let indexPaths = [indexPath]
-            tableView.deleteRows(at: indexPaths, with: .automatic)
-        }
+//        tableView(_ tableView: UITableView, commit, editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//
+//            itemArray.remove(at: indexPath.row)
+//            let indexPaths = [indexPath]
+//            tableView.deleteRows(at: indexPaths, with: .automatic)
+//        }
         
 //        cell.(nil)
         
@@ -48,6 +66,11 @@ class NyuryokuViewController: UIViewController, UITextFieldDelegate , UITableVie
 
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        itemArray.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+        
         if editingStyle == UITableViewCell.EditingStyle.delete {
             
             tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
@@ -80,13 +103,13 @@ class NyuryokuViewController: UIViewController, UITextFieldDelegate , UITableVie
         let action = UIAlertAction(title: "リストに追加", style: .default) {
             (action) in
 
-            let item = itemArray[indexPath.row]
+         let newItem = itemArray[indexPath.row]
 
-            let newItem: Item = Item(title: textField.text!)
+       let newItem: Item = Item(title: textField.text!)
+           self.itemArray.append(newItem)
             
-            self.itemArray.append(newItem)
             self.tableView.reloadData()
-            }
+          }
         
         alert.addTextField {
             (alertTextField) in
@@ -96,6 +119,13 @@ class NyuryokuViewController: UIViewController, UITextFieldDelegate , UITableVie
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+//        tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//
+//            itemArray.remove(at: indexPath.row)
+//            let indexPaths = [indexPath]
+//            tableView.deleteRows(at: indexPaths, with: .automatic)
+        
     }
     
     
--- timeschedule/PageViewController.swift
+++ timeschedule/PageViewController.swift
@@ -8,48 +8,27 @@
 
 import UIKit
 
-class PageViewController: UIViewController {
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setViewControllers([getFirst()], direction: .Forward, aanimated: true, completion: nil)
+        self.setViewControllers([getFirst()], direction: .forward, animated: true, completion: nil)
         self.dataSource = self
-
-        // Do any additional setup after loading the view.
     }
+        // Do any additional setup after loading the view.
+
     
     func getFirst() -> ViewController
     {
         return storyboard!.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    
-    extension PageViewController : UIPageViewControllerDataSource {
-        
-        func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewcontroller viewController: UIViewController) -> UIViewController? {
-            return nil
-        }
-        func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewcontroller viewController: UIViewController) -> UIViewController? {
-            if viewController.isKind(of: ViewController()) {
-                return getSecond()
-            }else if viewController.isKind(of: NyuryokuViewController()) {
-                return getThird()
-            }else {
-            return nil
-        }
     }
     
     func getSecond() -> NyuryokuViewController
@@ -62,4 +41,65 @@ class PageViewController: UIViewController {
         return storyboard!.instantiateViewController(withIdentifier: "GraphViewController") as! GraphViewController
     }
 
-}
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+//    if viewController is NyuryokuViewController{
+//        return getSecond()
+//    }else if viewController is ViewController{
+//        return getFirst()
+//    }else {
+//      return nil
+//}
+//
+//func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewcontroller viewController: UIViewController) -> UIViewController? {
+//    if viewController is  ViewController{
+//        return getSecond()
+//    }else if viewController is NyuryokuViewController{
+//        return getThird()
+//    }else {
+//    return nil
+//}
+//}
+//}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+//    extension PageViewController : UIPageViewControllerDataSource {
+        
+//        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+//            if viewController.isKindOfClass(GraphViewController) {
+//                return getSecond()
+//            }else if viewController.isKindOfClass(NyuryokuViewController)
+//            {
+//                return getFirst()
+//            }else {
+//              return nil
+//        }
+//
+//        func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewcontroller viewController: UIViewController) -> UIViewController? {
+//            if viewController.isKind(of: ViewController()) {
+//                return getSecond()
+//            }else if viewController.isKind(of: NyuryokuViewController()) {
+//                return getThird()
+//            }else {
+//            return nil
+//        }
+//    }
+    
+
+    }
+
+
+
