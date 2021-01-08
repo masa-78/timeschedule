b997c80dc4de2449a60f18bd57fe81e913107855
Masahiro Tonogai <tonomasa36@gmail.com>
2021-01-08 16:36:37 +0900

    円グラフを作り、textFieldに値を入力して％を変えられるようにしました

--- timeschedule/GraphViewController.swift
+++ timeschedule/GraphViewController.swift
@@ -10,15 +10,47 @@ import UIKit
 
 class GraphViewController: UIViewController {
 
+    private let textRate:UITextView = UITextView()
+        private let labelRate:UILabel = UILabel()
+        private let buttonDraw:UIButton = UIButton()
+        private let chartView:ChartView = ChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        textRate.layer.cornerRadius = 10
+                textRate.layer.borderColor = UIColor.lightGray.cgColor
+                textRate.layer.borderWidth = 0.5
+                textRate.keyboardType = .numberPad
+                textRate.text = "50"
+                textRate.font = UIFont.systemFont(ofSize: 16)
+                self.view.addSubview(textRate)
+        labelRate.text = "%"
+                self.view.addSubview(labelRate)
+                buttonDraw.setTitle("グラフ表示", for: .normal)
+                buttonDraw.setTitleColor(UIColor.blue, for: .normal)
+                buttonDraw.addTarget(self, action: #selector(self.touchUpButtonDraw), for: .touchUpInside)
+                self.view.addSubview(buttonDraw)
+
+                self.view.addSubview(chartView)
+
+                changeScreen()
         // Do any additional setup after loading the view.
     }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+            super.viewWillTransition(to: size, with: coordinator)
+            coordinator.animate(
+                alongsideTransition: nil,
+                completion: {(UIViewControllerTransitionCoordinatorContext) in
+                    self.changeScreen()
+            }
+            )
+        }
     
     override func viewWillAppear(_ animated: Bool) {
        super.viewWillAppear(animated)
        print("GraphViewController Will Appear")
+        drawChart()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -30,6 +62,36 @@ class GraphViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    private func changeScreen(){
+            let screenSize: CGRect = UIScreen.main.bounds
+            let widthValue = screenSize.width
+            let heightValue = screenSize.height
+    
+        textRate.frame = CGRect(x: widthValue/2-170, y: 50, width: 100, height: 40)
+                labelRate.frame = CGRect(x: widthValue/2-70, y: 50, width: 40, height: 40)
+                buttonDraw.frame = CGRect(x: widthValue/2-30, y: 50, width: 200, height: 40)
+
+                var drawWidth = widthValue * 0.8
+                if (widthValue > heightValue){
+                    drawWidth = heightValue * 0.8
+                }
+   
+        chartView.frame = CGRect(x: widthValue/2-drawWidth/2, y: 150, width: drawWidth, height: drawWidth)
+
+            }
+
+            @objc func touchUpButtonDraw(){
+                drawChart()
+            }
+
+            /**
+             グラフを表示
+             */
+            private func drawChart(){
+                let rate = Double(textRate.text!)
+                chartView.drawChart(rate: rate!)
+            }
+}
 // override func viewWillDisappear(_ animated: Bool ) {
 //       super.viewWillDisappear (animated: true)
 //        print("GraphViewController Will Disappear")
@@ -47,4 +109,4 @@ class GraphViewController: UIViewController {
     }
     */
 
-}
+
